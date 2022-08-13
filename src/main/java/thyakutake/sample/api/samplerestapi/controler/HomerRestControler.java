e5cdf1892abb7d2d433aa6dc477270f5192528bd
thyakutake <hyakutake_tatsuo@retail-ai.jp>
2022-08-13 14:51:04 +0900

    first push

---
+++ src/main/java/thyakutake/sample/api/samplerestapi/controler/HomerRestControler.java
@@ -0,0 +1,20 @@
+package thyakutake.sample.api.samplerestapi.controler;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class HomerRestControler {
+    @RequestMapping(value="/resthello")
+    String hello(){
+        return """
+                Hello
+                It works!
+                現在時刻は %s です
+                """.formatted(LocalDateTime.now());
+    }
+
+}
+
---
+++ src/test/java/thyakutake/sample/api/samplerestapi/SampleRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package thyakutake.sample.api.samplerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
